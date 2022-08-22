7d127c247bbf1927fdf83aff49c1c32deb778779
Thejus <git@tigerraw22.anonaddy.com>
2022-08-22 13:02:07 +0530

    Executed the flask backend and is work now need to work on frontend

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 import pickle
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/prediction',methods=['GET','POST'])
+
+@app.route('/prediction', methods=['GET', 'POST'])
 def prediction():
-    prediction_value = model.predict([[request.form.get('area')]])
-    prediction_value = round(prediction[0],2)
-    return render_template('index.html',prediction_text=f'Rate of the house is {prediction_value}/-')
+
+    area = request.form.get('area')
+    bathrooms = request.form.get('bathrooms')
+    stories = request.form.get('stories')
+    airconditioning = 1 if request.form.get('airconditioning') == 'Yes' else 0
+    parking = request.form.get('parking')
+
+    prediction_value = model.predict([[int(area), int(bathrooms),
+                                    int(stories), airconditioning, int(parking)]])
+    prediction_value = round(prediction_value[0], 2)
+
+    return render_template('index.html', prediction_text=f'Rate of the house is {prediction_value}/-')
 
 
 if __name__ == '__main__':
